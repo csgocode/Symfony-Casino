7da98d7e7412687f3ad088635b41c95da350c7a9
Toni <antoniostab68@gmail.com>
2023-10-18 10:43:47 +0200

    Update

--- src/Controller/ControladorCasinoController.php
+++ src/Controller/ControladorCasinoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Usuarios;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +15,17 @@ class ControladorCasinoController extends AbstractController
     {
         return $this->render('controlador_casino/index.html.twig', [
             'controller_name' => 'ControladorCasinoController',
+        ]);
+    }
+
+    #[Route('/checkUser/{DNI}', name: 'checkUser_DNI')]
+    public function checkUser(ManagerRegistry $doctrine, $dni): Response
+    {
+        $repositorio = $doctrine->getRepository(Usuarios::class);
+        $usuario = $repositorio->find($dni);
+
+        return $this->render('checks/DNIcheck.html.twig', [
+            'usuario' => $usuario
         ]);
     }
 }
